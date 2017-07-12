38d3a1abe9db23a5da2e6dfa2dba06861502fdeb
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-11 21:26:56 -0500

    git updated router

--- src/core/router.js
+++ src/core/router.js
@@ -4,16 +4,17 @@ import config from '../config';
 export default (app) => {
 
   //-> api/version/example: push in noLoad 'example' or renamed api/version/_example
-  fs.readdirSync(`${config.base}/api`).forEach(version => {
-    fs.readdirSync(`${config.base}/api/${version}`).forEach(route => {
-      if (config.router.ignore.indexOf(route) < 0)
-        if (route.charAt(0) !== '_')
-          app.use(`/api/${version}/${route}`, require(`../api/${version}/${route}`).default);
-    });
- });
+  // fs.readdirSync(`${config.base}/api`).forEach(version => {
+    // fs.readdirSync(`${config.base}/api/`).forEach(route => {
+    //   if (config.router.ignore.indexOf(route) < 0)
+    //     if (route.charAt(0) !== '_')
+    //       app.use(`/api/${route}`, require(`../api/${route}`).default);
+    // });
+ // });
 
   // Routers Manual
-  app.use('/auth', require('../lib/auth').default);
+  // app.use('/api/auth', require('../lib/auth').default);
+  app.use('/auth', require('../auth').default);
   // app.use('/api/version/hello', require('../api/version/hello'));
 
 };
