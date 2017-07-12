7e434560b13fce2f7ea25e8a1cf204b2092a27d3
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-11 21:25:46 -0500

    swaggerized app

--- src/app.js
+++ src/app.js
@@ -1,22 +1,50 @@
 import express from 'express';
 import http from 'http';
+import swaggerTools from 'swagger-tools';
 import chalk from 'chalk';
 import config from './config';
 const app = express();
-// Core
-require('./core/engine').default(app);
-// Routers
-require('./core/router').default(app);
+import * as auth from './lib/auth/swaggerMiddleware';
+
 // Swagger
-require('./core/swagger').default(app);
-// Paths
-require('./core/path').default(app);
-// MongoDB
-require('./core/mongoose');
-// Create HTTP server.
-const server = http.createServer(app);
-// Listen Server
-server.listen(config.server.port, config.server.ip, () => {
-  process.env.NODE_ENV = config.mode;
-  console.log(chalk.greenBright(`----------\nServer-> listening on http://${config.server.ip}:${config.server.port} in mode [${config.mode}]\n----------`));
+let swaggerConfig = require('./core/swagger').default(app);
+// console.log(swaggerConfig);
+// var YAML = require("yamljs");
+// let swaggerConfig = YAML.load(config.base + "/api/swagger/swagger.yaml");
+
+swaggerTools.initializeMiddleware(swaggerConfig, (middleware) => {
+  // Core
+  require('./core/engine').default(app);
+  // Routers
+  require('./core/router').default(app);
+  // MongoDB
+  require('./core/mongoose');
+
+  app.use(middleware.swaggerMetadata());
+
+  app.use(middleware.swaggerSecurity({
+    Bearer: auth.verifyToken
+  }));
+
+  let routerConfig = {
+    controllers: [config.base + '/api/controllers'],
+    // controllers: [config.base + '/api/controllers',config.base + '/lib/auth'],
+    useStubs: false
+  };
+
+  app.use(middleware.swaggerRouter(routerConfig));
+  // router.use(middleware.swaggerUi());
+
+  // Paths
+  require('./core/path').default(app);
+  // // Create HTTP server.
+  const server = http.createServer(app);
+  // // Listen Server
+  server.listen(config.server.port, config.server.ip, () => {
+    process.env.NODE_ENV = config.mode;
+    console.log(chalk.greenBright(
+      `\n\n-------\nServer-> listening on http://${config.server.ip}:${config.server.port} in mode [${config.mode}]\n-------`
+    ));
+  });
+
 });
