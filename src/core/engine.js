2976c9985ae34aaf5c4c7c107bb04367929c8f42
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-11 21:26:18 -0500

    updated engine

--- src/core/engine.js
+++ src/core/engine.js
@@ -11,6 +11,8 @@ import config from './../config';
 
 export default(app) => {
 
+
+
   app.use(bodyParser.json({limit: '5mb'}));
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(fileUpload());
