489d237d16ba3db7d7b5baf135e8bf7996efc531
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-19 21:19:26 -0500

    updated index

---
+++ src/auth/services/index.js
@@ -0,0 +1,26 @@
+import passport from 'passport';
+import config from '../../config';
+
+// Call back login Social return info in req.user
+
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+//
+// passport.deserializeUser((user, done) => {
+//   done(null, user);
+// });
+
+//Passport
+
+if (config.oAuth.local.enabled) require('../passports/local');
+
+if (config.oAuth.github.enabled) require('../passports/github');
+
+if (config.oAuth.twitter.enabled) require('../passports/twitter');
+
+if (config.oAuth.facebook.enabled) require('../passports/facebook');
+
+if (config.oAuth.google.enabled) require('../passports/google');
+
+if (config.oAuth.bitbucket.enabled) require('../passports/bitbucket');
