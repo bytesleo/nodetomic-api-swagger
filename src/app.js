3aaa44f521b01899de41ab71ea64071888c8efee
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-19 21:03:59 -0500

    updated app.js

--- src/app.js
+++ src/app.js
@@ -4,47 +4,25 @@ import swaggerTools from 'swagger-tools';
 import chalk from 'chalk';
 import config from './config';
 const app = express();
-import * as auth from './lib/auth/swaggerMiddleware';
-
 // Swagger
-let swaggerConfig = require('./core/swagger').default(app);
-// console.log(swaggerConfig);
-// var YAML = require("yamljs");
-// let swaggerConfig = YAML.load(config.base + "/api/swagger/swagger.yaml");
-
+let swaggerConfig = require('./core/swagger/config').default(app);
+// Init
 swaggerTools.initializeMiddleware(swaggerConfig, (middleware) => {
   // Core
   require('./core/engine').default(app);
-  // Routers
+  // Routers Express
   require('./core/router').default(app);
-  // MongoDB
+  // Mongo
   require('./core/mongoose');
-
-  app.use(middleware.swaggerMetadata());
-
-  app.use(middleware.swaggerSecurity({
-    Bearer: auth.verifyToken
-  }));
-
-  let routerConfig = {
-    // controllers: [config.base + '/api/controllers'],
-    controllers: [config.base + '/api/controllers',config.base + '/auth/controllers'],
-    useStubs: false
-  };
-
-  app.use(middleware.swaggerRouter(routerConfig));
-  // app.use(middleware.swaggerUi());  /docs
-
+  // Swagger
+  require('./core/swagger').default(app, swaggerConfig, middleware);
   // Paths
   require('./core/path').default(app);
-  // // Create HTTP server.
+  // Create HTTP server.
   const server = http.createServer(app);
-  // // Listen Server
+  // Listen Server
   server.listen(config.server.port, config.server.ip, () => {
     process.env.NODE_ENV = config.mode;
-    console.log(chalk.greenBright(
-      `\n\n-------\nServer-> listening on http://${config.server.ip}:${config.server.port} in mode [${config.mode}]\n-------`
-    ));
+    console.log(chalk.greenBright(`\n\n-------\nServer-> listening on http://${config.server.ip}:${config.server.port} in mode [${config.mode}]\n-------`));
   });
-
 });
