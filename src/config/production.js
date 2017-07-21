bd7ba7f4dc85502e4f33e1aca7b71479bd385999
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-21 11:23:13 -0500

    updated config to production

--- src/config/production.js
+++ src/config/production.js
@@ -1,7 +1,7 @@
 // development || production
 const mode = 'production';
 // Project Name:  mongodb://localhost:27017/{NAME}-{mode}
-const name = 'nodetomic-api';
+const name = 'your-app-name';
 import path from 'path';
 
 export default {
