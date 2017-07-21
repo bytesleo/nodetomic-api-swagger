c92c0a59f61a260aee733faf99c901add8295f1e
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-21 10:29:24 -0500

    updated helper

--- src/auth/services/middleware.js
+++ src/auth/services/middleware.js
@@ -1,4 +1,4 @@
-import api from 'express-easy-helper';
+import {unauthorized, forbidden} from 'express-easy-helper';
 import {verify} from '../../lib/token';
 import {get as redis, ttl} from '../../lib/redis';
 import {decrypt, hasRole} from '../../lib/utility';
@@ -6,13 +6,6 @@ import {decrypt, hasRole} from '../../lib/utility';
 // verifyToken
 export async function verifyToken(req, authOrSecDef, token, cb) {
 
-  function sendNotLoggedIn() {
-    return api.unauthorized(req.res, {message: 'Unauthorized'});
-  }
-
-  function sendError() {
-    return api.forbidden(req.res, {message: 'Access Denied'});
-  }
   //these are the scopes/roles defined for the current endpoint
   let currentScopes = req.swagger.operation["x-security-scopes"];
   // console.log('Require:', currentScopes);
@@ -22,12 +15,12 @@ export async function verifyToken(req, authOrSecDef, token, cb) {
     //Verify Token
     let decode = await verify(token);
     if (!decode)
-      return cb(sendError());
+      return cb(forbidden(req.res));
     let key = `${decode._id}:${decode._verify}`;
     //Extract info from redis
     let _user = await redis(key);
     if (!_user)
-      return cb(sendNotLoggedIn());
+      return cb(unauthorized(req.res));
 
     //decrypt redis
     _user = await JSON.parse(decrypt(_user));
@@ -37,16 +30,16 @@ export async function verifyToken(req, authOrSecDef, token, cb) {
     _user.key = key;
     //Same ids
     if (_user._id !== decode._id)
-      return cb(sendError());
+      return cb(forbidden(req.res));
 
     //Verify Roles
     if (currentScopes)
       if (!hasRole(currentScopes, _user.roles))
-        return cb(sendError());
+        return cb(forbidden(req.res));
   //Success
     req.user = _user;
     return cb(null);
   } else {
-    return cb(sendError());
+    return cb(forbidden(req.res));
   }
 }
