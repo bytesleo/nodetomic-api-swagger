692f7f47f69cc20106b57f8eae1aa5f185475eab
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-21 19:46:13 -0500

    redis sessions

---
+++ src/lib/redis/rs.js
@@ -0,0 +1,97 @@
+import Redis from 'redis-fast-driver';
+import config from '../../config';
+
+const r = new Redis(config.redis.rs);
+require('./status').default(r, config.redis.rs);
+
+// Create
+export async function create(key, value, ttl) {
+  try {
+    let id = key.split(':')[0];
+    if (!config.redis.multiple)
+      await destroyMultiple(id);
+    if (ttl) {
+      await r.rawCall(['SETEX', key, ttl, value]);
+    } else {
+      await r.rawCall(['SET', key, value]);
+    }
+  } catch (err) {
+    throw `Error 1 Redis ${err}`;
+  }
+  return true;
+}
+
+// exits by key
+export function exits(key) {
+  return new Promise((resolve, reject) => {
+    r.rawCall([
+      'EXISTS', key
+    ], (err, result) => {
+      if (result)
+        resolve(result);
+      resolve(null);
+    });
+  });
+}
+
+//Get ttl
+export function ttl(key) {
+  return new Promise((resolve, reject) => {
+    r.rawCall([
+      'ttl', key
+    ], (err, result) => {
+
+      if (result)
+        resolve(result);
+      resolve(null);
+    });
+  });
+}
+
+//Get by Pattern
+export function getValuesByPattern(pattern) {
+  return new Promise((resolve, reject) => {
+    r.rawCall([
+      'keys', `${pattern}:*`
+    ], (err, keys) => {
+      let command = ['MGET']
+      let query = command.concat(keys);
+      r.rawCall(query, (err, result) => {
+        if (result)
+          resolve(result);
+        resolve(null);
+      })
+    });
+  });
+}
+
+//Destroy by key
+export function destroy(key) {
+  return new Promise((resolve, reject) => {
+    r.rawCall([
+      'DEL', key
+    ], (err, result) => {
+      if (result)
+        resolve(result);
+      resolve(null);
+    });
+  });
+}
+
+//Destroy multiple by key
+export function destroyMultiple(key) {
+  return new Promise((resolve, reject) => {
+    r.rawCall([
+      'KEYS', `${key}:*`
+    ], (err, keys) => {
+      keys.join();
+      keys.splice(0, 0, "DEL");
+      r.rawCall(keys, (err, result) => {
+        if (result)
+          resolve(result);
+        resolve(null);
+      })
+    });
+  });
+
+}
