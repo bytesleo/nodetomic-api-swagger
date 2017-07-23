1a68446c75f26339090f68d20b639574c2c321f9
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-22 20:13:55 -0500

    updated facebook

--- src/auth/controllers/facebook.js
+++ src/auth/controllers/facebook.js
@@ -1,12 +1,14 @@
 import passport from 'passport';
 import {initialize} from '../services/session';
 
+// Init passport
 export function index(req, res, next) {
 
   passport.authenticate('facebook')(req, res, next);
 
 }
 
+// Callback passport
 export function callback(req, res, next) {
 
   passport.authenticate('facebook', (err, user) => initialize(err, user, res))(req, res, next);
