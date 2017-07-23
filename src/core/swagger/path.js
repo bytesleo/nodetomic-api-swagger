006d8a0c2012ccc09a98f29a0c2de5b50264b0b2
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-22 20:18:28 -0500

    renamed to path

---
+++ src/core/swagger/path.js
@@ -0,0 +1,20 @@
+import config from '../../config';
+
+export default (app, swaggerConfig) => {
+
+  // Delete Paths from /api-docs
+  if (config.oAuth) {
+    Object.keys(config.oAuth).forEach(elem => {
+      if (elem !== 'local') {
+        delete swaggerConfig.paths[`/auth/${elem}`]
+        delete swaggerConfig.paths[`/auth/${elem}/callback*`]
+      }
+    });
+  }
+  // Example
+  // delete swaggerConfig.paths['/auth/github'];
+
+  // If Swagger is enabled then the router is enabled!
+  if (config.swagger.enabled)
+    app.get(`/swagger.json`, (req, res) => res.json(swaggerConfig));
+};
