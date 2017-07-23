f58161b6ed1fa7428eaac9b407e028f45efe57cb
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-22 20:15:47 -0500

    updated require for passports

--- src/auth/services/index.js
+++ src/auth/services/index.js
@@ -1,26 +1,21 @@
-import passport from 'passport';
 import config from '../../config';
-
-// Call back login Social return info in req.user
-
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
-//
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
 
 //Passport's
 
-if (config.oAuth.local.enabled) require('../passports/local');
+if ("local" in config.oAuth && config.oAuth.local.enabled)
+  require('../passports/local');
 
-if (config.oAuth.github.enabled) require('../passports/github');
+if ("github" in config.oAuth && config.oAuth.github.enabled)
+  require('../passports/github');
 
-if (config.oAuth.twitter.enabled) require('../passports/twitter');
+if ("twitter" in config.oAuth && config.oAuth.twitter.enabled)
+  require('../passports/twitter');
 
-if (config.oAuth.facebook.enabled) require('../passports/facebook');
+if ("facebook" in config.oAuth && config.oAuth.facebook.enabled)
+  require('../passports/facebook');
 
-if (config.oAuth.google.enabled) require('../passports/google');
+if ("google" in config.oAuth && config.oAuth.google.enabled)
+  require('../passports/google');
 
-if (config.oAuth.bitbucket.enabled) require('../passports/bitbucket');
+if ("bitbucket" in config.oAuth && config.oAuth.bitbucket.enabled)
+  require('../passports/bitbucket');
