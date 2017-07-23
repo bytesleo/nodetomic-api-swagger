4f3b7f61ac04d804e106badd0576d871c70c8c86
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-22 20:21:28 -0500

    updated status mongoose

--- src/core/mongoose/status.js
+++ src/core/mongoose/status.js
@@ -2,25 +2,28 @@ import chalk from 'chalk';
 
 export default(db, config) => {
 
+  const uri = config.database.mongo.db.uri;
+
   // When successfully connected
   db.on('connected', (err) => {
-    console.log(chalk.greenBright(`-------\nMongoDB-> connected on ${config.database.mongo.db.uri}\n-------`));
+    console.log(chalk.greenBright(`-------\nMongoDB-> connected on ${uri}\n-------`));
   });
 
+  // When Open connection
   db.once('open', () => {
     // Plant seed
     require('./seed').default(db, config);
   });
 
   // If the connection throws an error
-  db.on('error',err => {
-      console.log(chalk.redBright(`MongoDB-> connection error: ${config.database.mongo.db.uri} details->${err}`));
-      process.exit(-1);
+  db.on('error', err => {
+    console.log(chalk.redBright(`MongoDB-> connection error: ${uri} details->${err}`));
+    process.exit(-1);
   });
 
   // When the connection is disconnected
-  db.on('disconnected',err => {
-      console.log(chalk.redBright(`MongoDB-> disconnected: ${config.database.mongo.db.uri}`));
+  db.on('disconnected', err => {
+    console.log(chalk.redBright(`MongoDB-> disconnected: ${uri}`));
   });
 
 };
