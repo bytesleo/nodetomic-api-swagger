07cde42b608bc1dbd0e92d6da944acd70fd09727
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-22 20:15:17 -0500

    updated passport local

--- src/auth/passports/local.js
+++ src/auth/passports/local.js
@@ -16,7 +16,7 @@ passport.use('local', new LocalStrategy({
     if (!user)
       return done(`${username}' is not registered.`); // You can register user here
 
-    user.authenticate(password).then((isMatch) => { //validate password
+    user.authenticate(password).then(isMatch => { //validate password
       if (!isMatch)
         return done('This password is not correct.');
       user.lastLogin = Date.now(); //save log last_login
