2bfc25cbec5ff4cca451e69bc0ebefd39fa4f4a7
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-22 20:21:06 -0500

    updated mongoose

--- src/core/mongoose/index.js
+++ src/core/mongoose/index.js
@@ -5,5 +5,5 @@ import config from '../../config';
 mongoose.Promise = require('bluebird');
 // Connect to database
 mongoose.connect(config.database.mongo.db.uri, config.database.mongo.db.options);
-// Status
+// Status connection
 require('./status').default(mongoose.connection, config);
