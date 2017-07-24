a8a2eb01f559fc76705e64eaebaa7edecea718d8
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-23 19:24:21 -0500

    added simple.config

---
+++ simple.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "apps": [
+    {
+      "name": "cleaner",
+      "script": "./server/cleaner/index.js",
+      // "watch": true
+      // "env": {
+      //   "NODE_ENV": "development"
+      // },
+      // "env_production": {
+      //   "NODE_ENV": "production"
+      // }
+    },
+    {
+      "name": "app",
+      "script": "./server/app.js"
+    }
+  ]
+}
