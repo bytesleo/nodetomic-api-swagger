625a6bc7e201ada27fca71b5caf528b6c5173c09
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-24 15:55:28 -0500

    updated build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,18 @@
+import fs from 'fs';
+import chalk from 'chalk';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import rename from 'gulp-rename';
 import clean from 'gulp-rimraf';
-import chalk from 'chalk';
 import minify from 'gulp-minifier';
 import replace_string from 'gulp-replace';
-import runSequence from 'run-sequence';
 import jeditor from "gulp-json-editor";
-import fs from 'fs';
+import runSequence from 'run-sequence';
 import config from './src/config';
 
 const dist = './dist';
 const dist_server = `${dist}/server`;
-const dist_swagger = `${dist}/api-docs`;
+const dist_swagger = `${dist_server}/core/swagger/api-docs`;
 const dist_package = `${dist}/package.json`;
 const dist_client = `${dist}/client`;
 const pm2_simple = `simple.config.js`;
@@ -24,12 +24,12 @@ gulp.task('build', () => {
 
 gulp.task('build-clean', () => {
   return gulp.src([
-    `${dist_server}/*`, `!${dist_server}/assets`, dist_swagger, dist_package
+    `${dist_server}/*`, `!${dist_server}/assets`, dist_package
   ], {read: false}).pipe(clean({force: true}));
 });
 
 gulp.task('build-babel', () => {
-  return gulp.src(['src/**/*.js', '!src/core/dev.js', '!src/config/*.js']).pipe(babel()).pipe(gulp.dest(dist_server));
+  return gulp.src(['src/**/*.js', '!src/core/dev.js', '!src/config/*.js', '!src/core/swagger/api-docs/*']).pipe(babel()).pipe(gulp.dest(dist_server));
 });
 
 gulp.task('build-replace', () => {
@@ -37,12 +37,12 @@ gulp.task('build-replace', () => {
   gulp.src(['src/views/**/*.{html,css}']).pipe(minify({minify: true, collapseWhitespace: true, conservativeCollapse: true, minifyJS: true, minifyCSS: true})).pipe(gulp.dest(`${dist_server}/views`));
   gulp.src(['src/assets/**/*']).pipe(gulp.dest(`${dist_server}/assets`));
   gulp.src(['src/**/*.yaml']).pipe(gulp.dest(dist_server));
-  gulp.src([`api-docs/*`]).pipe(gulp.dest(dist_swagger));
+  gulp.src([`src/core/swagger/api-docs/*`]).pipe(gulp.dest(dist_swagger));
   gulp.src(`${dist_server}/core/engine.js`).pipe(replace_string("require('./dev').default(app);", '')).pipe(gulp.dest(`${dist_server}/core`));
   gulp.src("package.json").pipe(jeditor((json) => {
     delete json.devDependencies;
     json.scripts = {
-      "start": `npm run redis && node server/cleaner & node server/app.js`,
+      "start": `npm run redis && node server/core/cleaner & node server/app.js`,
       "pm2-simple": `npm run redis && pm2 start ${pm2_simple}`,
       "pm2-cluster": `npm run redis && pm2 start ${pm2_cluster}`,
       "pm2-dev-simple": `pm2 kill && npm run pm2-simple && pm2 monit`,
