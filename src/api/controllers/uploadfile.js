5290afcd613b5f7776f95621a6cf1b81c3ed0e61
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-24 16:05:39 -0500

    renamed file

---
+++ src/api/controllers/uploadfile.js
@@ -0,0 +1,24 @@
+import {result, error} from 'express-easy-helper';
+import config from '../../config';
+
+// Upload a file
+export function index(req, res) {
+  try {
+    if (req.files) {
+      //file with name 'example' in form
+      let file = req.files.example;
+      let path = `/assets/${new Date().getTime()}_${file.name}`;
+
+      file.mv(`${config.base}/${path}`, err => {
+        if (err)
+          throw err;
+        result(res, 201, {message: `file upload: ${path}`});
+      });
+
+    } else {
+      throw 'Not found files!';
+    }
+  } catch (err) {
+    error(res, err);
+  }
+}
