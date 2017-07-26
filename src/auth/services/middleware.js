655fed40370a951ac3e26b2d93c9ed2492924c87
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:20:57 -0500

    decode data from db

--- src/auth/services/middleware.js
+++ src/auth/services/middleware.js
@@ -1,56 +1,58 @@
 import {unauthorized, forbidden} from 'express-easy-helper';
-import {verify} from '../../lib/token';
-import {exits as rsExits, ttl as rsTtl} from '../../lib/redis/sessions';
-import {get as ruGet} from '../../lib/redis/users';
-import {decrypt} from '../../lib/utility/crypto';
+import {exits as rExits, ttl as rTtl} from '../../lib/redis';
 import {hasRole} from '../../lib/utility/role';
+import {verify} from '../../lib/token';
+import User from '../../api/models/user';
 
 // VerifyToken
 export async function verifyToken(req, authOrSecDef, token, cb) {
 
-  //these are the scopes/roles defined for the current endpoint
+  // these are the scopes/roles defined for the current endpoint
   let currentScopes = req.swagger.operation["x-security-scopes"];
 
   if (token) {
     // Bearer
     req.headers.authorization = `Bearer ${token}`;
-    //Verify Token
+
+    // Verify Token
     let decode = await verify(token);
     if (!decode)
       return cb(forbidden(req.res));
 
+    // Key session in redis
     let key = `${decode._id}:${decode._verify}`;
 
     // Verify if exits token in redis
-    if (!await rsExits(key))
+    if (!await rExits(key))
       return cb(unauthorized(req.res));
 
-    // Extract info user from redis
-    let _user = await ruGet(decode._id);
+    // Extract info user from MongoDB
+    let _user = await User.findById(decode._id).select('-social').exec();
     if (!_user)
       return cb(unauthorized(req.res));
 
-    //decrypt redis
-    _user = await JSON.parse(decrypt(_user));
+    // If id's not equals
+    if (_user._id.toString() !== decode._id.toString())
+      return cb(forbidden(req.res));
 
-    //user is enabled?
+    // User is enabled?
     if (!_user.status)
       return cb(unauthorized(req.res));
 
-    //set TTL
-    _user.ttl = await rsTtl(key);
-    //set key
-    _user.key = key;
-    //If id's not equals
-    if (_user._id !== decode._id)
-      return cb(forbidden(req.res));
-
-    //Verify Roles
+    // Verify Roles
     if (currentScopes)
       if (!hasRole(currentScopes, _user.roles))
         return cb(forbidden(req.res));
-  //Success
-    req.user = _user;
+
+    // get current TTL
+    let ttl = await rTtl(key);
+
+    // Success
+    req.user = Object.assign({
+      key,
+      ttl
+    }, _user._doc);
+
     return cb(null);
   } else {
     return cb(forbidden(req.res));
