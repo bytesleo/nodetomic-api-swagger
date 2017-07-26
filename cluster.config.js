7df57dbe0255e7ec211f8d730b9dc7d3e29b9aa1
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:17:29 -0500

    updated Cluster config

--- cluster.config.js
+++ cluster.config.js
@@ -3,22 +3,17 @@ const numCPUs = require('os').cpus().length;
 module.exports = {
   "apps": [
     {
-      "name": "cleaner",
-      "script": "./server/core/cleaner/index.js",
-      // "watch": true
-      // "env": {
-      //   "NODE_ENV": "development"
-      // },
-      // "env_production": {
-      //   "NODE_ENV": "production"
-      // }
-    },
-    {
       "name": "app",
       "script": "./server/app.js",
       "instances": numCPUs,
       "exec_mode": "cluster",
-      // "watch": true
+      "watch": false,
+      "env": {
+        "NODE_ENV": "development"
+      },
+      "env_production": {
+        "NODE_ENV": "production"
+      }
     }
   ]
 }
