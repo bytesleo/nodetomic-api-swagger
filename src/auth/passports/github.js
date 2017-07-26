dfcaf1d5af5e7dfc7ba9bed4ea91f3ac66975d96
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:26:50 -0500

    updated callback

--- src/auth/passports/github.js
+++ src/auth/passports/github.js
@@ -24,10 +24,13 @@ passport.use(new GitHubStrategy({
     } else {
       user.social.info = profile._json;
       user.photo = profile._json.avatar_url;
-      user.lastLogin = Date.now();
     }
 
-    user.save().then(user => done(null, user)).catch(err => done(err));
+    user.lastLogin = Date.now();
+
+    user.save().then(_user => {
+      return done(null, _user);
+    }).catch(err => done(err));
 
   }).catch(err => done(err));
 
