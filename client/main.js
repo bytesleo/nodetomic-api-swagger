23c5d90e75a1d1186be6fba16b540a4ad45d4989
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:17:14 -0500

    added routers

--- client/main.js
+++ client/main.js
@@ -1,7 +1,23 @@
 var host = location.origin;
-new Vue({
-  el: '#nodetomicApp',
-  data: {
-    project: 'API'
+
+const home = {
+  template: '<div><div class="project">API</div><div class="docs"><a href="/docs">Docs Here!</a></div></div>'
+}
+
+const token = {
+  template: '<div class="token"><div>Token</div><textarea>' + Cookies.get('token') + '</textarea></div>'
+}
+
+const routes = [
+  {
+    path: '/',
+    component: home
+  }, {
+    path: '/token',
+    component: token
   }
-});
+]
+
+const router = new VueRouter({routes})
+
+const app = new Vue({router}).$mount('#app')
