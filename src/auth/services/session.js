daa28cbaa66810a8a0eff2c57ba2fe03380ec583
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:21:24 -0500

    deleted methods and updated

--- src/auth/services/session.js
+++ src/auth/services/session.js
@@ -1,10 +1,8 @@
 import {result, invalid, error} from 'express-easy-helper';
 import {create as jwtCreate} from '../../lib/token';
-import {create as rsCreate} from '../../lib/redis/sessions';
-import {create as ruCreate, get as ruGet, update as ruSet, destroy as ruDestroy} from '../../lib/redis/users';
+import {create as rCreate} from '../../lib/redis';
 import {makeid} from '../../lib/utility/makeid';
 import {ttl} from '../../lib/utility/ttl';
-import {encrypt, decrypt} from '../../lib/utility/crypto';
 
 // Initialize after login success
 export async function initialize(err, user, res) {
@@ -14,23 +12,8 @@ export async function initialize(err, user, res) {
   if (!user)
     return error(res, {message: 'Something went wrong, please try again.'});
 
-  // Values to save in redis user (ru)
-  let _user = {
-    _id: user._id,
-    username: user.username,
-    name: user.name,
-    lastname: user.lastname,
-    email: user.email,
-    photo: user.photo,
-    provider: user.provider,
-    roles: user.roles,
-    status: user.status,
-    date: user.date,
-    lastLogin: user.lastLogin
-  }
-
-  // Values to save in redis session (rs)
-  let _session = {
+  // Values to save in redis session
+  let session = {
     verify: makeid(11),
     agent: res.req.headers['user-agent'],
     ip: res.req.headers['x-forwarded-for'] || res.req.connection.remoteAddress
@@ -38,14 +21,20 @@ export async function initialize(err, user, res) {
 
   let token = null;
   try {
+
     // Create Token and save in Redis
-    token = await create(_user, _session);
-    // Local
-    if (_user.provider === 'local')
-      return result(res, {token});
-    // Social
+    token = await create(user, session);
+
+    // Save token in cookies
     res.cookie('token', JSON.stringify(token));
-    res.redirect('/');
+
+    // if local return token
+    if (user.provider === 'local')
+      return result(res, {token});
+
+    // if Social redirect
+    res.redirect('/#/token');
+
   } catch (err) {
     return error(res, {message: `Error creating keys in redis ${err}`});
   }
@@ -54,40 +43,18 @@ export async function initialize(err, user, res) {
 
 // Create keysand value in redis
 async function create(user, session) {
+  // Id user
+  let _id = user._id.toString();
   // Init Jwt
-  let token = await jwtCreate(user, session.verify);
+  let token = await jwtCreate(_id, session.verify);
   // Create key in redis for session
-  let _key = `${user._id.toString()}:${session.verify}`;
-  // Encrypt info user
-  let _user = encrypt(JSON.stringify(user));
+  let _key = `${_id}:${session.verify}`;
   // Stringify info session
   let _session = JSON.stringify(session);
-  // Calculate ttl by user rol
+  // Calculate ttl by user role
   let _ttl = ttl(user.roles);
   // Create Session in redis
-  await rsCreate(_key, _session, _ttl);
-  // Create User in redis
-  await ruCreate(_key, _user);
+  await rCreate(_key, _session, _ttl);
   // Return token
   return token;
-}
-
-// If the user model is updated it is synchronized with redis
-export async function update(id, userUpdated) {
-  // Get info user from redis
-  let userOld = await ruGet(id);
-  // Parse info to JSON
-  userOld = await JSON.parse(decrypt(userOld));
-  // Merge info old and new info
-  let merge = Object.assign(userOld, userUpdated);
-  // Encrypt merge info
-  let user = encrypt(JSON.stringify(merge));
-  // Set info in redis
-  return await ruSet(id, user);
-}
-
-// If the user model is destroyed clean with redis
-export async function destroy(id) {
-  // Destroy user from redis
-  return await ruDestroy(id);
 }
