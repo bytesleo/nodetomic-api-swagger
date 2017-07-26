2cf15d297738ac6b3d7b25fed81e89636405713e
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:17:54 -0500

    updated Simple config

--- simple.config.js
+++ simple.config.js
@@ -1,19 +1,15 @@
 module.exports = {
   "apps": [
     {
-      "name": "cleaner",
-      "script": "./server/core/cleaner/index.js",
-      // "watch": true
-      // "env": {
-      //   "NODE_ENV": "development"
-      // },
-      // "env_production": {
-      //   "NODE_ENV": "production"
-      // }
-    },
-    {
       "name": "app",
-      "script": "./server/app.js"
+      "script": "./server/app.js",
+      "watch": false,
+      "env": {
+        "NODE_ENV": "development"
+      },
+      "env_production": {
+        "NODE_ENV": "production"
+      }
     }
   ]
 }
