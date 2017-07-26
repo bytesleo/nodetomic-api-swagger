dace9ad4260791bc16bb4a131dc96cda839ed906
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:19:01 -0500

    removed comments

--- src/core/dev.js
+++ src/core/dev.js
@@ -7,16 +7,7 @@ export default(app) => {
   if (config.livereload.enabled)
     app.use(require('connect-livereload')({src: `http://${config.livereload.ip}:${config.livereload.port}/livereload.js`}));
 
-  if (config.log) {
-    app.use(morgan('dev', {
-      //            skip: function (req, res) {
-      //                if (res.statusCode == 304) {
-      //                    return true;
-      //                }else{
-      //                    return false;
-      //                }
-      //                return res.statusCode == 304;
-      //            }
-    }));
-  }
+  if (config.log)
+    app.use(morgan('dev'));
+
 };
