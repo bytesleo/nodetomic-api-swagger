f165c9d0b1971b224373acf6182f4f486682ae43
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-25 19:23:21 -0500

    added test for authentication

---
+++ test/auth.js
@@ -0,0 +1,24 @@
+const request = require("request");
+let n = 0;
+
+setInterval(function() {
+  var options = {
+    method: 'POST',
+    url: 'http://localhost:8000/auth/local',
+    headers: {
+      'postman-token': '7adf01b2-9d2e-0bb4-38b9-037fa55f472b',
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+      username: 'user',
+      password: '123'
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error)
+      throw new Error(error);
+    n++;
+    console.log(`#${n} - ${body}`);
+  });
+}, 30);
