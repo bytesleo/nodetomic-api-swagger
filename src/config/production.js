1e5ae608ba7093a32dfcf6a7358f233b8eaa3944
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-27 17:19:54 -0500

    updated config to production

--- src/config/production.js
+++ src/config/production.js
@@ -18,10 +18,10 @@ export default {
   // Roles: if a user has multiple roles, will take the time of the greater role
   roles : [
     {
-      rol: 'user',
+      role: 'user',
       time: 60, // minutes
     }, {
-      rol: 'admin',
+      role: 'admin',
       time: 'infinite'
     }
   ],
@@ -135,5 +135,5 @@ export default {
     ip: 'localhost',
     port: 35729
   },
-  log : true // Log request in console?
+  log : false // Log request in console?
 };
