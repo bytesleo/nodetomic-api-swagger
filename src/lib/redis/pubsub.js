cf352e52ef3a72b973c59356fbc8ef4044a632ac
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-29 15:25:24 -0500

    updated pubsub with examples

--- src/lib/redis/pubsub.js
+++ src/lib/redis/pubsub.js
@@ -2,51 +2,68 @@ import Redis from 'redis-fast-driver';
 import chalk from 'chalk';
 import config from '../../config';
 
-const pubsub = new Redis(config.redis.sessions);
-require('./status').default(pubsub, config.redis.sessions, 'pubsub');
+const r = new Redis(config.redis.sessions.conn);
+require('./status').default(r, config.redis.sessions.conn, 'pubsub');
 
-const node = process.env.NODE_APP_INSTANCE || 0;
+// Get CLuster or simple Process
+const node = parseInt(process.env.NODE_APP_INSTANCE) || 0;
 
-// observer
-export async function observer(key) {
+/*-----------
+* Basic Exmaple Pubsub
+*/
 
-  // 'subscribe', `__keyevent@${config.redis.sessions.db}__:del`, `__keyevent@${config.redis.sessions.db}__:expired`
-  // 'psubscribe', `__key*__:${key}:*`
+// Listener all nodes
+r.rawCall([
+  'subscribe', `news`
+], async function (e, data) {
+  console.log(chalk.blueBright(`${data} - node = ${node}`));
+});
 
-  pubsub.rawCall([
-    'psubscribe', `__key*__:${key}`
-  ], async function(e, data) {
-    // key, action, id
-    let obj = get(data);
-    let msg = '';
-    if (obj) {
-      switch (obj.action) {
-        case 'expired':
-          msg = `expired ${obj.key}`;
-          break;
-        case 'del':
-          msg = `del ${obj.key}`;
-          break;
-        default:
-          break;
+// Send publish from node 0
+if (node === 0) {
+  const r2 = new Redis(config.redis.sessions.conn);
+  setTimeout(function (){
+    r2.rawCall(['PUBLISH', 'news', 'hello world!']);
+  }, 3000);
+}
+/*-----------*/
+
+
+// Thread event manager (Expired, del)
+if (config.redis.sessions.pubsub.events) {
+  if (node === 0) {
+    r.rawCall([
+      'subscribe', `__keyevent@${config.redis.sessions.conn.db}__:del`, `__keyevent@${config.redis.sessions.conn.db}__:expired`
+    ], async function (e, data) {
+
+      let obj = get(data);
+      let msg = '';
+      if (obj) {
+        switch (obj.event) {
+          case 'expired':
+            msg = `expired ${obj.key}`;
+            break;
+          case 'del':
+            msg = `del ${obj.key}`;
+            break;
+          default:
+            break;
+        }
+      } else {
+        msg = data;
       }
-    } else {
-      msg = data;
-    }
-
-    if (config.log)
-      console.log(chalk.blueBright(`${msg} - node = ${node}`));
-
-  });
+      if (config.log)
+        console.log(chalk.blueBright(`${msg} - node = ${node}`));
+    });
+  }
 }
 
 // Format event
 function get(data) {
-  if (data[0] === 'pmessage') {
-    let key = data[1].split('__key*__:')[1];
-    let id = key.split(':')[0];
-    let action = data.reverse()[0];
-    return {key, action, id};
+  if (data[0] === 'message') {
+    let event = data[1].split(`__keyevent@${config.redis.sessions.conn.db}__:`)[1];
+    let key = data.reverse()[0];
+    return { event, key };
   } else {
     return null;
   }
