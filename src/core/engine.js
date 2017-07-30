6136439a36d0076105a29f63b8c839b42a8c3483
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-30 15:57:01 -0500

    indent file

--- src/core/engine.js
+++ src/core/engine.js
@@ -10,26 +10,26 @@ import fileUpload from 'express-fileupload';
 import morgan from 'morgan';
 import config from './../config';
 
-export default(app) => {
+export default (app) => {
 
-  app.use(bodyParser.json({limit: '5mb'}));
-  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(fileUpload());
   app.use(cookieParser());
   app.use(methodOverride());
   app.use(compression());
   app.use(helmet());
-  app.use(cors({origin: true, credentials: true}));
+  app.use(cors({ origin: true, credentials: true }));
 
   if ("twitter" in config.oAuth && config.oAuth.twitter.enabled)
-    app.use(session({secret: config.secret, resave: false, saveUninitialized: false}));
+    app.use(session({ secret: config.secret, resave: false, saveUninitialized: false }));
 
   app.use(passport.initialize());
   app.use(passport.session());
 
   // Liverload
   if (config.livereload.enabled)
-    app.use(require('connect-livereload')({src: `http://${config.livereload.ip}:${config.livereload.port}/livereload.js`}));
+    app.use(require('connect-livereload')({ src: `http://${config.livereload.ip}:${config.livereload.port}/livereload.js` }));
 
   // Morgan
   if (config.log)
