4e4dfdc89be29336c204a7fe02c93a0c4a755062
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-30 16:00:58 -0500

    updated lib/socket.io

--- src/lib/socket.io/index.js
+++ src/lib/socket.io/index.js
@@ -1,25 +1,45 @@
-import redis from 'socket.io-redis';
+import Redis from 'socket.io-redis';
+import chalk from 'chalk';
 import fs from "fs";
 import config from '../../config';
 
 const io = require('socket.io')(config.server.port + 1);
-io.adapter(redis({ host: 'localhost', port: 6379 }));
-const node = parseInt(process.env.NODE_APP_INSTANCE) || 0;
+io.adapter(Redis(config.redis.sockets.conn));
 
 // Scan events
-
 const pathSocket = `${config.base}/api/sockets`;
 let events = [];
 fs.readdirSync(pathSocket).forEach(path => {
     events.push(`${pathSocket}/${path}`);
 });
 
+// Instance
+const node = parseInt(process.env.NODE_APP_INSTANCE) || 0;
 
-io.on('connection', function (socket) {
+// Init
+io.on('connection', socket => {
 
-    console.log(`Socket Connected - node - ${node}`);
+    if (config.log) {
+        console.log(chalk.magentaBright(`Socket connected - node = ${node}`));
+        total();
+    }
+
     // Autoload sockets
     events.forEach(path => require(path).default(socket, io));
+
+    socket.on('disconnect', () => {
+        if (config.log) {
+            console.log(chalk.magentaBright(`Socket disconnect - node = ${node}`));
+            total();
+        }
+    });
+
 });
 
+// Total socket clients
+function total() {
+    io.of('/').adapter.clients((err, clients) => {
+        console.log(chalk.magentaBright(`Socket total clients = ${clients.length}`));
+    });
+}
 
