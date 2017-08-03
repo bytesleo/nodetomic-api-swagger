3f254a446e4034e9a86e7f4b0a480b85e0200bee
Leonardo Rico <leonardo.ricog@gmail.com>
2017-08-02 19:12:04 -0500

    change location to lib

---
+++ src/lib/mongoose/seed.js
@@ -0,0 +1,40 @@
+import chalk from 'chalk';
+
+export default (connection, config) => {
+
+  // Get list collections
+  connection.db.listCollections().toArray((err, collections) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // Get seeds in config
+      config.database.mongo.db.seeds.forEach(seed => {
+        // let model = seed.path.split('/').reverse()[0].match(/\S+(?=.seed)/g)[0] + 's'; //format [model.seed.js]
+        let model = seed.path.split('/').reverse()[0] + 's'; //format [model.js]
+        // Evaluate seed
+        switch (seed.plant) {
+          case 'once':
+            connection.db.collection(model).count((err, count) => {
+              if (count <= 0) {
+                connection.db.dropCollection(model, (err, result) => {
+                  console.log(chalk.cyan(`Seed-> Sowing seed ${model}...`));
+                  require(`${config.base}${seed.path}`);
+                });
+              }
+            });
+            break;
+          case 'alway':
+            connection.db.dropCollection(model, (err, result) => {
+              console.log(chalk.cyan(`Seed-> Sowing seed ${model}...`));
+              require(`${config.base}${seed.path}`);
+            });
+            break;
+          case 'never':
+            //console.log(chalk.magentaBright(`Seed-> ${model} never`));
+            break;
+          default:
+        }
+      });
+    }
+  });
+}
