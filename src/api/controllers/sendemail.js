76313e84100f1df05c4ec5fbbfcee28d2db365bb
Leonardo Rico <leonardo.ricog@gmail.com>
2017-08-09 19:55:40 -0500

    udpated util

--- src/api/controllers/sendemail.js
+++ src/api/controllers/sendemail.js
@@ -1,6 +1,6 @@
 import { result, error } from 'express-easy-helper';
-import { send } from '../../lib/utility/nodemailer';
-import { getTemplate, setTemplate } from '../../lib/utility/hogan';
+import { send } from '../../lib/util/nodemailer';
+import { getTemplate, setTemplate } from '../../lib/util/hogan';
 const templateExample = getTemplate('templates/example.html'); //Example with .html, .mustache, .js
 
 // Send a email
