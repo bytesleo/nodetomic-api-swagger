db84e1b2f817b52703fe5c6484034e159e36deb9
Leonardo Rico <leonardo.ricog@gmail.com>
2017-08-09 19:54:30 -0500

    added async to mongoose

--- src/lib/mongoose/index.js
+++ src/lib/mongoose/index.js
@@ -1,10 +1,35 @@
 import mongoose from 'mongoose';
+import chalk from 'chalk';
 import config from '../../config';
 
-// mongoose.Promise = global.Promise;
-mongoose.Promise = require('bluebird');
-// Connect to database
-// mongoose.connect(config.database.mongo.db.uri, config.database.mongo.db.options);
-mongoose.connect(config.database.mongo.db.uri, { useMongoClient: true });
-// Status connection
-require('./status').default(mongoose.connection, config);
+const uri = config.database.mongo.db.uri;
+const opts = config.database.mongo.db.options;
+
+mongoose.Promise = global.Promise;
+
+export async function connect() {
+
+    try {
+        let conn = await mongoose.connect(uri, opts);
+
+        // Events
+        conn.on('disconnected', (err) => {
+            console.log(chalk.redBright(`MongoDB-> disconnected: ${uri}`));
+        });
+
+        conn.on('reconnected', (err) => {
+            console.log(chalk.greenBright(`MongoDB-> reconnected: ${uri}`));
+        });
+
+        // Success
+        console.log(chalk.greenBright(`-------\nMongoDB-> connected on ${uri}\n-------`));
+
+        // Plant seed
+        require('./seed').default(conn, config);
+
+    } catch (err) {
+        console.log(chalk.redBright(`MongoDB-> connection error: ${uri} details->${err}`));
+        process.exit(-1);
+    }
+
+}
