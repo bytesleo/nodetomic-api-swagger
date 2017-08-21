e6d1b70af4fb4aaa74ce32cd00b4a69c11bf43dd
Leonardo Rico <leonardo.ricog@gmail.com>
2017-08-21 14:47:55 -0500

    updated subpub

--- src/lib/redis/pubsub.js
+++ src/lib/redis/pubsub.js
@@ -5,23 +5,21 @@ import config from '../../config';
 const r = new Redis(config.redis.sessions.conn);
 require('./status').default(r, config.redis.sessions.conn, 'pubsub');
 
-// Get CLuster or simple Process
-const node = parseInt(process.env.NODE_APP_INSTANCE) || 0;
-
 /*
 * Basic Example Pubsub
 * Listener all nodes
 */
+
 r.rawCall([
   'subscribe', `news`
 ], async function (e, data) {
 
   if (config.log)
-    console.log(chalk.blueBright(`${data} - node = ${node}`));
+    console.log(chalk.blueBright(`${data} - node = ${config.node}`));
 });
 
 // Send publish from node 0
-if (node === 0) {
+if (config.node === 0) {
   const r2 = new Redis(config.redis.sessions.conn);
   setTimeout(function () {
     r2.rawCall(['PUBLISH', 'news', 'hello world!']);
@@ -31,33 +29,34 @@ if (node === 0) {
 
 
 // Thread event manager (Expired, del)
-if (config.redis.sessions.pubsub.events) {
-  if (node === 0) {
-    r.rawCall([
-      'subscribe', `__keyevent@${config.redis.sessions.conn.db}__:del`, `__keyevent@${config.redis.sessions.conn.db}__:expired`
-    ], async function (e, data) {
+/*
+if (node === 0) {
+  r.rawCall([
+    'subscribe', `__keyevent@${config.redis.sessions.conn.db}__:del`, `__keyevent@${config.redis.sessions.conn.db}__:expired`
+  ], async function (e, data) {
 
-      let obj = get(data);
-      let msg = '';
-      if (obj) {
-        switch (obj.event) {
-          case 'expired':
-            msg = `expired ${obj.key}`;
-            break;
-          case 'del':
-            msg = `del ${obj.key}`;
-            break;
-          default:
-            break;
-        }
-      } else {
-        msg = data;
+    let obj = get(data);
+    let msg = '';
+    if (obj) {
+      switch (obj.event) {
+        case 'expired':
+          msg = `expired ${obj.key}`;
+          break;
+        case 'del':
+          msg = `del ${obj.key}`;
+          break;
+        default:
+          break;
       }
-      if (config.log)
-        console.log(chalk.blueBright(`${msg} - node = ${node}`));
-    });
-  }
+    } else {
+      msg = data;
+    }
+    if (config.log)
+      console.log(chalk.blueBright(`${msg} - node = ${node}`));
+  });
 }
+*/
+
 
 // Format event
 function get(data) {
