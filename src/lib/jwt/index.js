3273e71621746d779185737c6c0f819dc0b0b1d8
Leonardo Rico <leonardo.ricog@gmail.com>
2017-08-21 23:46:13 -0500

    updated catch

--- src/lib/jwt/index.js
+++ src/lib/jwt/index.js
@@ -5,28 +5,24 @@ import config from '../../config';
 // Create Token
 export async function create(_id, _verify) {
 
-  let token = null;
   try {
-    token = await jwt.sign({
+    return await jwt.sign({
       _id,
       _verify
     }, config.secret);
   } catch (err) {
-    throw 'Error creating token';
+    return false;
   }
-  return token;
 
 }
 
 // Verify Token
 export async function verify(token) {
 
-  let isMatch = null;
   try {
-    isMatch = jwt.verify(token, config.secret);
+    return jwt.verify(token, config.secret);
   } catch (err) {
-    isMatch = false;
+    return false;
   }
-  return isMatch;
 
 }
