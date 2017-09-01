f5de6b8168e99f23bf2b0c58218a1cd9bd482cf2
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-01 16:29:06 -0500

    updated mongoose

--- src/lib/mongoose/index.js
+++ src/lib/mongoose/index.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import chalk from 'chalk';
 import config from '../../config';
 
-const uri = config.mongo.db.uri;
-const opts = config.mongo.db.options;
+const uri = config.mongoose.uri;
+const opts = config.mongoose.options;
 
 mongoose.Promise = global.Promise;
 
