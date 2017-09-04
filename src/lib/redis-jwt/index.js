c6ef579fa8548013a701823fdbed8e28f9f0a7b5
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-04 01:48:18 -0500

    renamed folders

--- src/lib/redis-jwt/index.js
+++
@@ -1,8 +0,0 @@
-import RedisJWT from 'redis-jwt';
-import config from '../../config';
-
-export const r = new RedisJWT(config['redis-jwt']);
-
-export const exec = r.exec();
-
-export const call = r.call();
