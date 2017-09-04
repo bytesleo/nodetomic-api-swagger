094a2b69cc96191c4a0f2840bb73fcdd235cdc37
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-04 14:52:03 -0500

    added async

--- src/app.js
+++ src/app.js
@@ -1,28 +1,41 @@
 import express from 'express';
-import swaggerTools from 'swagger-tools';
 import chalk from 'chalk';
 import config from './config';
 const app = express();
 
-async function run() {
-  // Express
-  await require('./core/express').index(app);
-  // Swagger Config
-  let swaggerConfig = await require('./lib/swagger/config').default(app);
-  // Swagger Middleware
-  swaggerTools.initializeMiddleware(swaggerConfig, middleware => {
-    // Swagger Init 
-    require('./lib/swagger').default(app, swaggerConfig, middleware);
-    // Paths
-    require('./core/path').default(app);
-    // Server
-    app.listen(config.server.port, config.server.ip, () => {
-      console.log(chalk.greenBright(`-------\nServer-> 
-      mode: [${chalk.magentaBright(`${config.mode}`)}]
-      url: ${chalk.blueBright(`http://${config.server.ip}:${config.server.port}`)}\n-------`));
-      console.log(chalk.black.bgGreenBright(`>>nodetomic-api-swagger ready!<<`));
-    });
-  });
-}
+(async function run() {
 
-run();
+  // Express
+  await require('./lib/express').index(app);
+
+  // Mongoo
+  await require('./lib/mongo').connect();
+
+  // Redis
+  await require('./lib/redis').connect();
+
+  // Socket
+  await require('./lib/socket').connect();
+
+  // Services
+  await require('./auth/services');
+
+  // Swagger
+  await require('./lib/swagger').index(app);
+
+  // Paths
+  await require('./lib/express/path').default(app);
+
+  // Server 
+  app.listen(config.server.port, config.server.ip, () => {
+    // Info
+    console.log(chalk.greenBright(`-------\nServer-> 
+          mode: [${chalk.magentaBright(`${config.mode}`)}]
+          url: ${chalk.blueBright(`http://${config.server.ip}:${config.server.port}`)}\n-------`));
+
+    // Ready!
+    console.log(chalk.black.bgGreenBright(`>>nodetomic-api-swagger ready!<<`));
+  });
+
+})();
+
