089dddd1f6fa406e04225e515f8c237a7001dd85
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:10:25 -0500

    updated spaces

--- src/auth/controllers/google.js
+++ src/auth/controllers/google.js
@@ -3,14 +3,10 @@ import { initialize } from '../services/session';
 
 // Init passport
 export function index(req, res, next) {
-
   passport.authenticate('google')(req, res, next);
-
 }
 
 // Callback passport
 export function callback(req, res, next) {
-
   passport.authenticate('google', (err, user) => initialize(err, user, res))(req, res, next);
-
 }
