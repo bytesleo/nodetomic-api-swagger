03022a68440f479c728a00890f7693dcabdd425e
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:10:43 -0500

    updated spaces

--- src/auth/controllers/twitter.js
+++ src/auth/controllers/twitter.js
@@ -3,14 +3,10 @@ import { initialize } from '../services/session';
 
 // Init passport
 export function index(req, res, next) {
-
   passport.authenticate('twitter')(req, res, next);
-
 }
 
 // Callback passport
 export function callback(req, res, next) {
-
   passport.authenticate('twitter', (err, user) => initialize(err, user, res))(req, res, next);
-
 }
