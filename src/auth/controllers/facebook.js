aac0e48ee9e54e34ddbf6b4dae96d7f40f13e532
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:10:37 -0500

    updated spaces

--- src/auth/controllers/facebook.js
+++ src/auth/controllers/facebook.js
@@ -3,14 +3,10 @@ import { initialize } from '../services/session';
 
 // Init passport
 export function index(req, res, next) {
-
   passport.authenticate('facebook')(req, res, next);
-
 }
 
 // Callback passport
 export function callback(req, res, next) {
-
   passport.authenticate('facebook', (err, user) => initialize(err, user, res))(req, res, next);
-
 }
