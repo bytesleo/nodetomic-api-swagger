22cf587f9932ef784971a587a7ba4465d7933f24
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:13:00 -0500

    flow updated

--- src/auth/passports/twitter.js
+++ src/auth/passports/twitter.js
@@ -9,28 +9,12 @@ passport.use(new TwitterStrategy({
   callbackURL: config.oAuth.twitter.callbackURL
 }, (token, tokenSecret, profile, done) => {
 
-  User.findOne({ provider: 'twitter', 'social.id': profile.id }).exec().then(user => {
+  let social = profile;
+  social.email = profile.emails[0].value;
+  social.photo = profile._json.profile_image_url;
 
-    if (!user) {
-      user = new User({
-        name: profile.displayName,
-        username: profile.username,
-        provider: 'twitter',
-        photo: profile._json.profile_image_url,
-        'social.id': profile.id,
-        'social.info': profile._json
-      });
-    } else {
-      user.social.info = profile._json;
-      user.photo = profile._json.profile_image_url;
-    }
-
-    user.lastLogin = Date.now();
-
-    user.save().then(_user => {
-      return done(null, _user);
-    }).catch(err => done(err));
-
-  }).catch(err => done(err));
+  User.loginBySocial('twitter', social)
+    .then(user => done(null, user))
+    .catch(err => done(err));
 
 }));
