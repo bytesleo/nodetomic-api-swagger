0e356e626f994b015d3a0a645fdaca27d96701a0
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:13:22 -0500

    flow updated

--- src/auth/passports/local.js
+++ src/auth/passports/local.js
@@ -8,25 +8,8 @@ passport.use('local', new LocalStrategy({
   //passReqToCallback: true
 }, function (username, password, done) {
 
-  User.findOne({
-    username: username,
-    provider: 'local'
-  }).select("+password").exec().then(user => {
-
-    if (!user)
-      return done(`${username}' is not registered.`); // You can register user here
-
-    user.authenticate(password).then(isMatch => { //validate password
-      if (!isMatch)
-        return done('This password is not correct.');
-
-      user.lastLogin = Date.now(); //save log last_login
-
-      user.save().then(_user => {
-        return done(null, _user);
-      }).catch(err => done(err));
-    });
-
-  }).catch(err => done(err));
+  User.loginByLocal(username, password)
+    .then(user => done(null, user))
+    .catch(err => done(err));
 
 }));
