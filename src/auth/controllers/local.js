c7c29705c5da819af229d03bf7d546702f13e3ca
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:11:05 -0500

    updated spaces

--- src/auth/controllers/local.js
+++ src/auth/controllers/local.js
@@ -3,7 +3,5 @@ import { initialize } from '../services/session';
 
 // Callback passport
 export function callback(req, res, next) {
-
   passport.authenticate('local', (err, user) => initialize(err, user, res))(req, res, next);
-
 }
