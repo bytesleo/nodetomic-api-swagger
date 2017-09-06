3936b397a3ecd1343925191549b19259a8256be6
Leonardo Rico <leonardo.ricog@gmail.com>
2017-09-06 17:09:47 -0500

    spaces

--- src/auth/controllers/bitbucket.js
+++ src/auth/controllers/bitbucket.js
@@ -3,14 +3,10 @@ import { initialize } from '../services/session';
 
 // Init passport
 export function index(req, res, next) {
-
   passport.authenticate('bitbucket')(req, res, next);
-
 }
 
 // Callback passport
 export function callback(req, res, next) {
-
   passport.authenticate('bitbucket', (err, user) => initialize(err, user, res))(req, res, next);
-
 }
