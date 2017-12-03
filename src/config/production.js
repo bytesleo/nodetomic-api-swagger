f9c77b7061d24f483c9f008e5ead2e00f67de2e8
Leonardo Rico <leonardo.ricog@gmail.com>
2017-12-03 08:52:36 -0500

    Added seed to Demo

--- src/config/production.js
+++ src/config/production.js
@@ -58,8 +58,8 @@ export default {
           plant: 'once' //  once - always - never
         },
         {
-          file: 'hello',
-          schema: 'Hello',
+          file: 'demo',
+          schema: 'Demo',
           plant: 'once'
         }
       ]
